264c0af3764a7709d3e982e832f78d28035e70c4
Murat <Murat-305@hotmail.com>
2019-05-06 17:54:15 -0600

    Login

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ var Movie = require('./Movies');
 var Comment = require('./Comments');
 mongodb = require('mongodb');
 ObjectId = mongodb.ObjectId;
-
-module.exports = app;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
